ce95b263e1032b488195c4c9707962570ca32030
Dushmanthr <dushmantha15ranathunga@gmail.com>
2023-12-30 03:00:07 +0530

    Add new arrival section

--- src/components/Clock/Clock.jsx
+++ src/components/Clock/Clock.jsx
@@ -11,7 +11,7 @@ const Clock = () => {
     let interval ;
 
     const countDown =()=>{
-        const destination = new Date('Dec 31, 2023').getTime
+        const destination = new Date('Dec 31, 2023').getTime();
         interval = setInterval(()=>{
 
             const now = new Date().getTime()
@@ -28,7 +28,7 @@ const Clock = () => {
             (1000))
 
             if(destination < 0){
-                clearInterval(interval.current)
+                clearInterval(interval)
             }
             else{
                 setDays(days)
@@ -41,33 +41,37 @@ const Clock = () => {
         })
     }
 
+    useEffect(()=>{
+      countDown();
+    })
+
   return (
     <div className="clock_wrapper d-flex align-items-center gap-3">
         <div className="clock_data d-flex align-items-center gap-3">
            <div className="text-center">
-             <h1 className="text-white fs-3 mb-2">10</h1>
-             <h5 className="text-white fs-6">{days}</h5>
+             <h1 className="text-white fs-3 mb-2">{days}</h1>
+             <h5 className="text-white fs-6">Days</h5>
             </div> 
             <span className="text-white fs-3">:</span>
         </div>
         <div className="clock_data d-flex align-items-center gap-3">
            <div className="text-center">
-             <h1 className="text-white fs-3 mb-2">10</h1>
-             <h5 className="text-white fs-6">{hours}</h5>
+             <h1 className="text-white fs-3 mb-2">{hours}</h1>
+             <h5 className="text-white fs-6">Hours</h5>
             </div> 
             <span className="text-white fs-3">:</span>
         </div>
         <div className="clock_data d-flex align-items-center gap-3">
            <div className="text-center">
-             <h1 className="text-white fs-3 mb-2">10</h1>
-             <h5 className="text-white fs-6">{minutes}</h5>
+             <h1 className="text-white fs-3 mb-2">{minutes}</h1>
+             <h5 className="text-white fs-6">Minutes</h5>
             </div> 
             <span className="text-white fs-3">:</span>
         </div>
         <div className="clock_data d-flex align-items-center gap-3">
            <div className="text-center">
-             <h1 className="text-white fs-3 mb-2">10</h1>
-             <h5 className="text-white fs-6">{seconds}</h5>
+             <h1 className="text-white fs-3 mb-2">{seconds}</h1>
+             <h5 className="text-white fs-6">Seconds</h5>
             </div> 
             {/* <span className="text-white fs-1">:</span> */}
         </div>
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -4,6 +4,7 @@ import Home_div from '../../components/Home_Div/Home_div'
 import Services from '../.././services/Services'
 import TreProducts from '../../components/Tre_products/Tre_products'
 import Timer from '../../components/Timer/Timer'
+import NewArrival from '../../components/New_Arrival/NewArrival'
 
 
 const Home = () => {
@@ -18,6 +19,9 @@ const Home = () => {
 
         <div>
           <Timer/>
+        </div>
+        <div>
+          <NewArrival/>
         </div>
         
     </div>
